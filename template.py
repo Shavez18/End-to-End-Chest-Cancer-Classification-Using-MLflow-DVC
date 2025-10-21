4a2a706fb3f7035d3324819327dc60cd88de1c00
Shavez18 <ahmedshavez892@gmail.com>
2025-10-21 20:25:53 +0530

    requirements added

--- template.py
+++ template.py
@@ -32,7 +32,7 @@ list_of_files = [
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath) 
 
 
     if filedir !="":
