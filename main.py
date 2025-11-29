c28c1373164e3821b2429fcee0d8d4cc874e4592
Shavez18 <ahmedshavez892@gmail.com>
2025-11-29 15:14:37 +0530

    model_evaluation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
+
 STAGE_NAME = "Data Ingestion stage"
 
 
